62cfc88a1b3eeb21241cb0c8dbc20f888c10b809
Linngeshwar <lingaeshrajan@gmail.com>
2024-09-24 20:28:00 +0530

    [update] added front end and backend to the app

--- src/components/Title.jsx
+++
@@ -1,9 +0,0 @@
-import React  from "react";
-
-function Title(){
-    return (<>
-        <div className="w-full text-[#000000] text-5xl font-semibold bg-transparent">Find Your Perfect Recipe</div>
-    </>)
-}
-
-export default Title
