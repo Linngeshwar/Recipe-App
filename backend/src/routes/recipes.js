b2705d4e0752142221a3a796c54125a86ea292a4
Linngeshwar <lingaeshrajan@gmail.com>
2024-10-01 01:51:22 +0530

    [fix] some stuff :)

--- backend/src/routes/recipes.js
+++ backend/src/routes/recipes.js
@@ -5,15 +5,39 @@ dotenv.config();
 const recipeRouter = express.Router();
 
 recipeRouter.get("/",async (req, res) => {
-  const appID = process.env.APP_ID;
-  const appKey = process.env.APP_KEY;
   const apiKey = process.env.API_KEY;
+  const apiKey2 = process.env.API_KEY2;
   const query = req.query.dishName;
-  const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&number=10&apiKey=${apiKey}`;
+  const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&number=1&apiKey=${apiKey2}`;
+try {
   const response = await fetch(url);
-  const data = await response.json();
-  
-  res.json(data);
+    if(response.ok) { 
+      const data = await response.json();
+      let recipeIDs = [];
+
+      data.results.forEach((recipe) => {
+        recipeIDs.push(recipe.id);
+      });
+
+      if(recipeIDs.length === 0) {
+        res.status(404).send("No recipes found");
+      }
+      const recipeData = await Promise.all(
+        recipeIDs.map(async (id) => {
+          const recipeResponse = await fetch(
+            `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=${apiKey2}`
+          );
+          const recipeData = await recipeResponse.json();
+          return recipeData;
+        }
+      ));
+      res.status(200).send(recipeData);
+    }else{
+      res.status(404).send("No recipes found");
+    }
+  } catch (error) {
+    console.error(error); 
+  }
 });
 
 module.exports = recipeRouter;
